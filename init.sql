6e30a7080f1f8ee804eaf5d4c5ecf30492579770
mi-mott <98304083+mi-mott@users.noreply.github.com>
2024-04-09 09:14:23 +1000

    Adding a table to the database, also inserting some data to use for testing

--- init.sql
+++ init.sql
@@ -1,1 +1,14 @@
 -- Add your schema initialisation script here if you're on Postgres and not using an ORM
+
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    create_date DATE NOT NULL DEFAULT CURRENT_DATE,
+    due_date DATE NOT NULL
+);
+
+INSERT INTO tasks (name, description, due_date) VALUES ('First', 'First desc', '2024-04-10');
+INSERT INTO tasks (name, description, due_date) VALUES ('Second', 'Second desc', '2024-04-10');
+INSERT INTO tasks (name, description, due_date) VALUES ('Third', 'Third desc', '2024-04-10');
+INSERT INTO tasks (name, description, due_date) VALUES ('Fourth', 'Fourth desc', '2024-04-10');
