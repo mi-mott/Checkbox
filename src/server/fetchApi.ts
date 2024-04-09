a23952e2ec5d3c8c06213c7c6b0f7344e06fbcf9
mi-mott <98304083+mi-mott@users.noreply.github.com>
2024-04-09 10:01:34 +1000

    Added .env variables into docker variable, changed the database initialization to use environment vars

--- src/server/fetchApi.ts
+++ src/server/fetchApi.ts
@@ -1,22 +1,27 @@
 import pg from 'pg';
 
-const { Client } = pg
-const client = new Client({ 
-  host: 'database',
-  port: 5432,
-  database: 'task',
-  user: 'root',
-  password: 'root',
-});
+const getNewClient = () => {
+    const { Client } = pg
+    const client = new Client({ 
+        host: 'database',
+        port: Number(process.env.POSTGRES_PORT),
+        database: process.env.POSTGRES_DB,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+    });
+    return client;
+}
 
 export const fetchTaskApi = async () => {
-  try {
-    await client.connect();
-    const queryResult = await client.query('SELECT * FROM tasks');
-    return queryResult.rows
-  } catch (error) {
-      console.error('Error executing query:', error);
-  } finally {
-      await client.end();
-  }
+    const client = getNewClient();
+
+    try {
+        await client.connect();
+        const queryResult = await client.query('SELECT * FROM tasks');
+        return queryResult.rows
+    } catch (error) {
+        console.error('Error executing query:', error);
+    } finally {
+        await client.end();
+    }
 }
