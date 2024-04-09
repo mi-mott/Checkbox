e67f536c84ae0e58ffac83d29844d4a7afee13b0
mi-mott <98304083+mi-mott@users.noreply.github.com>
2024-04-09 18:56:36 +1000

    Removing stub insertion code

--- init.sql
+++ init.sql
@@ -4,11 +4,6 @@ CREATE TABLE tasks (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
-    create_date DATE NOT NULL DEFAULT CURRENT_DATE,
-    due_date DATE NOT NULL
+    create_date TIMESTAMP NOT NULL DEFAULT CURRENT_DATE,
+    due_date TIMESTAMP NOT NULL
 );
-
-INSERT INTO tasks (name, description, due_date) VALUES ('First', 'First desc', '2024-04-10');
-INSERT INTO tasks (name, description, due_date) VALUES ('Second', 'Second desc', '2024-04-10');
-INSERT INTO tasks (name, description, due_date) VALUES ('Third', 'Third desc', '2024-04-10');
-INSERT INTO tasks (name, description, due_date) VALUES ('Fourth', 'Fourth desc', '2024-04-10');
