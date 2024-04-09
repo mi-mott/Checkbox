4e7ebefda166dce5471c6014e497e1818629e67f
mi-mott <98304083+mi-mott@users.noreply.github.com>
2024-04-09 18:58:39 +1000

    Express endpoints complete

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,19 +1,46 @@
 import express from 'express';
 import ViteExpress from 'vite-express';
-import pg from 'pg';
-import { fetchTaskApi } from './fetchApi.js';
+import { dbPostNewTask, dbGetAllTasks, dbDeleteTask, dbUpdateTask, dbQueryTaskByName } from './db.js';
 
 const app = express();
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json()); 
 
-// For fetching the tasks with no parameters
+// Get request for all tasks
 app.get('/fetchTasks', async (_, res) => {
-  const tasks = await fetchTaskApi();
+  const tasks = await dbGetAllTasks();
   res.json({ results: tasks })
 });
 
+// Get request based off search parameter
+app.get('/fetchByTaskName', async (req, res) => {
+  const taskName = req.query.taskName as string;
+  const tasks = await dbQueryTaskByName(taskName);
+  res.json({ results: tasks })
+})
+
+// Post to update via a id
+app.post('/newTask', async (req, res) => {
+  const {taskName, taskDesc, dueDate} = req.body;
+  const newTaskId = await dbPostNewTask(taskName, taskDesc, dueDate)
+  res.json({ id: newTaskId })
+})
+
+// Delete to delete any finished tasks
+app.delete('/deleteTask', async (req, res) => {
+  const {taskId} = req.body;
+  const deletedTaskId = await dbDeleteTask(taskId)
+  res.json({ id: deletedTaskId })
+})
+
+// Put to update any tasks
+app.put('/updateTask', async (req, res) => {
+  const {taskName, taskDesc, dueDate, taskId} = req.body;
+  const updatedTask = await dbUpdateTask(taskName, taskDesc, dueDate, taskId)
+  res.json({ id: updatedTask })
+})
+
 ViteExpress.config({
-  // Copy and paste of vite.config.ts just so vite-express does not need to import
-  // vite, a devDependency, in runtime
   inlineViteConfig: {
     build: {
       outDir: './dist/client',
