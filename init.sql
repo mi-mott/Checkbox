be3e77059aa1e8425ca735ae1ed9b71cd3db62ca
mi-mott <98304083+mi-mott@users.noreply.github.com>
2024-04-09 19:23:49 +1000

    Changed the allowed lengths for name and desc, frontend also enforces these rules

--- init.sql
+++ init.sql
@@ -1,9 +1,7 @@
--- Add your schema initialisation script here if you're on Postgres and not using an ORM
-
 CREATE TABLE tasks (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(150) NOT NULL,
     create_date TIMESTAMP NOT NULL DEFAULT CURRENT_DATE,
     due_date TIMESTAMP NOT NULL
 );
